8fc91187667244a2be89e7e820a4c588af436e57
rekagea <reka.meszaros@gea.com>
2023-11-21 18:23:16 +0100

    Organize layout

--- frontend/src/pages/Archive.jsx
+++ frontend/src/pages/Archive.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Flex, Box } from '@chakra-ui/react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Sidebar from '../components/Sidebar';
 
 const ArchivePage = () => {
   return (
-    <div>
+    <Flex direction="column" minH="100vh">
       <Header />
-      <Sidebar />
-      
+
+      <Flex flex="1">
+        <Sidebar />
+        {/* Add your archive content here */}
+        <Box p="4">
+          <h1>Archive Content Goes Here</h1>
+        </Box>
+      </Flex>
+
       <Footer />
-    </div>
+    </Flex>
   );
 };
 
